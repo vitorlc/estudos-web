003385687bba8ae4fa6f098e0e0481c8900092be
vitorlc <vitorcirqueira@inf.ufg.br>
2018-11-09 11:14:13 -0200

    entendimento this usando self

---
+++ javascript/thisEBind.1.js
@@ -0,0 +1,11 @@
+function Pessoa (){
+    const self = this
+    this.idade = 0
+
+    setInterval(function(){
+        self.idade++
+        console.log(self.idade)
+    }/*.bind(this)*/,1000)
+}
+
+new Pessoa
