cd40bf7ada46e9b7a2ed176f92755902f881bf12
gabriel <gabrielrsp18@gmail.com>
2019-10-22 23:02:25 -0300

    adicionando exemplo e comentarios sobre arrow function

--- javascript/arrowFunction1.js
+++ javascript/arrowFunction1.js
@@ -6,6 +6,7 @@ let dobro = function (a){
 dobro = (a) => { //se usa { } é obrigado o uso do return
     return 2*a
 }
+// Se possui apenas 1 variável não precisa de parentesis, e quando só uma 1 única linha vc faz uma função pequena e mais específica e o retorno está implícito
 
 //Usando Arrow Function com return implícito
 dobro = a => 2 * a
@@ -15,3 +16,15 @@ console.log(dobro(Math.PI))
 ola = () => 'Olá'
 ola = _ => 'Olá'
 console.log(ola())
+
+
+//Outro exemplo
+
+function Pessoa(){
+    this.idade = 0
+
+    setInterval( () => {
+        this.idade++
+        console.log(this.idade)
+    },1000)
+}
